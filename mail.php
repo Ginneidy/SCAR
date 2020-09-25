259c0f0581c3d284f25c7dd3d84e5f5aa149ff37
Ginneidy <64049555+Ginneidy@users.noreply.github.com>
2020-09-25 16:17:52 -0500

    Recepcion de correos
    implementacion de phpmailer

--- mail.php
+++ mail.php
@@ -1,31 +1,89 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMail/Exception.php';
+require './PHPMail/PHPMailer.php';
+require './PHPMail/SMTP.php';
+$mail = new PHPMailer(true);
+if (!isset($_POST['submit'])) {
+    echo "<p>Debes llenar el formulario</p>";
+    exit;
+}
+if (isset($_POST['submit'])) {
     $to = 'rasud@udistrital.edu.co';
     $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $phone= $_POST["phone"];
-    
+    $email = $_POST["email"];
+    $text = $_POST["message"];
+    $phone = $_POST["phone"];
+    if (empty($firstname) || empty($email) || empty($text) || empty($phone)) {
+        echo "<script>alert('Todos los datos son obligatorios')</script>";
+        exit;
+    }
+    function IsInjected($str)
+    {
+        $injections = array(
+            '(\n+)',
+            '(\r+)',
+            '(\t+)',
+            '(%0A+)',
+            '(%0D+)',
+            '(%08+)',
+            '(%09+)'
+        );
+        $inject = join('|', $injections);
+        $inject = "/$inject/i";
+        if (preg_match($inject, $str)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if (IsInjected($email)) {
+        echo "<script>alert('email invalido')</script>";
+        exit;
+    }
+    try {
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
 
+        //Server settings
+        $mail->SMTPDebug = 0;                       //Ver errores
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';       // SMTP de gmail
+        $mail->SMTPAuth   = true;                   // Enable SMTP authentication
+        $mail->Username   = '';                     // Correo del que envia, debe tener configurada la privacidad
+        $mail->Password   = '';                     // Contraseña del correo
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;                    // TCP puerto de gmail
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        //Recipients
+        $mail->setFrom(''); // El que envia el correo
+        $mail->addAddress(''); //El que lo recibe 
 
-    $message ='<table style="width:100%">
+        // Content
+        $mail->isHTML(true);                                  // Si tiene html en caso de venir de un fomrulario
+        $mail->Subject = 'Incripcion a congreso'; //Asunto del correo
+        $mail->Body    = '<table style="width:100%">
         <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
+            <td>' . $firstname . '  ' . $laststname . '</td>
         </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
+        <tr><td>Email: ' . $email . '</td></tr>
+        <tr><td>phone: ' . $phone . '</td></tr>
+        <tr><td>Text: ' . $text . '</td></tr>
         
     </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
+        //cuerpo
+        $mail->send();
+        echo "<script>alert('Inscripción realizada exitosamente')</script>";
+        echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
-
-?>
+}
